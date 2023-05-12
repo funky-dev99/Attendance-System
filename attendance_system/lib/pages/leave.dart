4bd500861706a99bbc2f6aa36513d31c76cfa07f
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-05-12 22:00:17 +0530

    leave page

--- attendance_system/lib/pages/leave.dart
+++ attendance_system/lib/pages/leave.dart
@@ -1,28 +1,274 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
-
 import 'home_page.dart';
 
 class Leave extends StatelessWidget {
-  const Leave({Key key}) : super(key: key);
+   Leave({Key key}) : super(key: key);
+   void setState(Null Function() param0) {}
 
+  final List<String> items = [
+    'Vacation',
+    'Sick',
+    'Holiday',
+    'Special Leave',
+
+
+  ];
+  // String selectedValue;
+   final List<String> day_part = [
+     'Full Day',
+     'Half Day',
+
+   ];
+   String selectedValue;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         backgroundColor: Colors.white,
         title: const Text(
-        "Leave Approval",
-        style: TextStyle(color: Colors.black, fontSize: 22),
-    ),
-          leading: IconButton(
-            icon: Image.asset("images/back.png"),
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => const HomePage()));
-            },
-          ),
-          elevation: 0.0,
+          "Leave Approval",
+          style: TextStyle(color: Colors.black, fontSize: 22),
         ),
+        leading: IconButton(
+          icon: Image.asset("images/back.png"),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => const HomePage()));
+          },
+        ),
+        elevation: 0.0,
+      ),
+      body: Column(
+        children:  [
+          Container(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: const Text("New Leave",
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w500,
+              ),),
+          ),
+          const SizedBox(height: 20,),
+
+          const Text("Leave Type",
+            style: TextStyle(
+              fontSize: 15.0,
+            ),),
+          const SizedBox(
+            height: 5.0
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton2(
+                isExpanded: true,
+                hint:  Row(
+                  children: const [
+                    // Icon(
+                    //   Icons.list,
+                    //   size: 16,
+                    //   color: Colors.black,
+                    // ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Expanded(
+                      child: Text(
+                        'Leave Type',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+                items: items
+                    .map((item) => DropdownMenuItem<String>(
+                  value: item,
+                  child: Text(
+                    item,
+                    style:  const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ))
+                    .toList(),
+                value: selectedValue,
+                onChanged: (value) {
+                  setState(() {
+                    selectedValue = value as String;
+                  });
+                },
+                buttonStyleData: ButtonStyleData(
+                  height: 50,
+                  width: 300,
+                  padding: const EdgeInsets.only(left: 14, right: 14),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.black26,
+                    ),
+                    color: Colors.white54,
+                  ),
+                  elevation: 2,
+                ),
+                iconStyleData: const IconStyleData(
+                  icon: Icon(
+                    Icons.arrow_drop_down_circle_outlined,
+                  ),
+                  iconSize: 14,
+                  iconEnabledColor: Colors.black,
+                  iconDisabledColor: Colors.grey,
+                ),
+                dropdownStyleData: DropdownStyleData(
+                  maxHeight: 200,
+                  width: 200,
+                  padding: null,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14),
+                    color: Colors.white54,
+                  ),
+                  elevation: 8,
+                  offset: const Offset(-20, 0),
+                  scrollbarTheme: ScrollbarThemeData(
+                    radius: const Radius.circular(40),
+                    thickness: MaterialStateProperty.all<double>(6),
+                    thumbVisibility: MaterialStateProperty.all<bool>(true),
+                  ),
+                ),
+                menuItemStyleData: const MenuItemStyleData(
+                  height: 40,
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                ),
+              ),
+            ),
+          ),
+
+          const Text("Day Part",
+            style: TextStyle(
+              fontSize: 15.0,
+            ),),
+          const SizedBox(
+              height: 5.0
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton2(
+                isExpanded: true,
+                hint:  Row(
+                  children: const [
+                    // Icon(
+                    //   Icons.list,
+                    //   size: 16,
+                    //   color: Colors.black,
+                    // ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Expanded(
+                      child: Text(
+                        'Day Part',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+                items: day_part
+                    .map((item) => DropdownMenuItem<String>(
+                  value: item,
+                  child: Text(
+                    item,
+                    style:  const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ))
+                    .toList(),
+                value: selectedValue,
+                onChanged: (value) {
+                  setState(() {
+                    selectedValue = value as String;
+                  });
+                },
+                buttonStyleData: ButtonStyleData(
+                  height: 50,
+                  width: 300,
+                  padding: const EdgeInsets.only(left: 14, right: 14),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.black26,
+                    ),
+                    color: Colors.white54,
+                  ),
+                  elevation: 2,
+                ),
+                iconStyleData: const IconStyleData(
+                  icon: Icon(
+                    Icons.arrow_drop_down_circle_outlined,
+                  ),
+                  iconSize: 14,
+                  iconEnabledColor: Colors.black,
+                  iconDisabledColor: Colors.grey,
+                ),
+                dropdownStyleData: DropdownStyleData(
+                  maxHeight: 200,
+                  width: 200,
+                  padding: null,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14),
+                    color: Colors.white54,
+                  ),
+                  elevation: 8,
+                  offset: const Offset(-20, 0),
+                  scrollbarTheme: ScrollbarThemeData(
+                    radius: const Radius.circular(40),
+                    thickness: MaterialStateProperty.all<double>(6),
+                    thumbVisibility: MaterialStateProperty.all<bool>(true),
+                  ),
+                ),
+                menuItemStyleData: const MenuItemStyleData(
+                  height: 40,
+                  padding: EdgeInsets.only(left: 14, right: 14),
+                ),
+              ),
+            ),
+          ),
+
+
+
+
+
+
+
+          // Text("Leave Type",
+          //   style: TextStyle(
+          //     fontSize: 15.0,
+          //   ),),
+
+
+
+        ],
+      ),
     );
   }
+
 }
