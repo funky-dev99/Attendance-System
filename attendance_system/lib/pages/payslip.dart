8a63c16fe5b415ef43cb2987ae9587d0e4d1c07a
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-06-02 10:20:15 +0530

    Pages Added

---
+++ attendance_system/lib/pages/payslip.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PaySlipUp extends StatelessWidget {
+  const PaySlipUp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
